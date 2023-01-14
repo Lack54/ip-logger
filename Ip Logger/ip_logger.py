5c7a7f0ab9239cc91cc3ee01e74175aabf3a0bf4
Lack <118399851+Lack54@users.noreply.github.com>
2023-01-14 17:55:26 -0500

    Update ip_logger.py

--- Ip Logger/ip_logger.py
+++ Ip Logger/ip_logger.py
@@ -1,7 +1,7 @@
 import requests
 
 def main():
-    r = requests.get("https://grabify.link/XJ80O3", timeout=2)
+    r = requests.get("IP LOGGING WEBSITE", timeout=2)
     time = str(r.elapsed).split(".")[:2]
     miliseconds = time[1][:2]
     print(f"LOGGED IN {miliseconds} MILISECONDS")
